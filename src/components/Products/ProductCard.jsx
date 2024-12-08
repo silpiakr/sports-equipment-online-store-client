8df5d444e6f0a0aa9ed595de73b2b246a3436130
silpiAkter <silpiakter461@gmail.com>
2024-12-08 21:44:18 +0600

    added product section

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,29 +1,29 @@
-// import React from 'react';
+import React from 'react';
 
-// const ProductCard = ({equipment, equipments, setEquipments}) => {
-//     const {name, photo, price, status} = equipment;
-//     return (
-//         <div>
-//             <div className="card bg-base-100 w-96 shadow-xl">
-//                 <figure>
-//                     <img
-//                         src={photo}
-//                         alt="" />
-//                 </figure>
-//                 <div className="card-body">
-//                     <h2 className="card-title">
-//                         {name}
-//                         <div className="badge badge-secondary">{status? status : 'N/A'}</div>
-//                     </h2>
-//                     <p>{price}</p>
-//                     <div className="card-actions justify-end">
-//                         <div className="badge badge-outline">{}</div>
-//                         <div className="badge badge-outline">{}</div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+const ProductCard = ({equipment, equipments, setEquipments}) => {
+    const {name, photo, price, status} = equipment;
+    return (
+        <div>
+            <div className="card bg-base-100 w-96 shadow-xl">
+                <figure>
+                    <img
+                        src={photo}
+                        alt="" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {name}
+                        <div className="badge badge-secondary">{status? status : 'N/A'}</div>
+                    </h2>
+                    <p>{price}</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">{}</div>
+                        <div className="badge badge-outline">{}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// export default ProductCard;
+export default ProductCard;
