6d3531c6844f2d544a5dac7a7e9c5d6b072e1826
silpiAkter <silpiakter461@gmail.com>
2024-12-08 21:44:55 +0600

    added product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,21 +1,21 @@
-// import React, { useEffect, useState } from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import ProductCard from './ProductCard';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import ProductCard from './ProductCard';
 
 
-// const Products = () => {
-//     const products = useLoaderData();
-//     const [equipments, setEquipments] = useState([]);
+const Products = () => {
+    const products = useLoaderData();
+    const [equipments, setEquipments] = useState([]);
  
-//     return (
-//         <div className='sm-w-[425px] md:max-w-7xl mx-auto '>
-//             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6'>
-//                {
-//                 equipments.map((equipment) => (<ProductCard key={product._id} product={equipment} equipments={equipments}></ProductCard>))
-//                }
-//             </div>
-//         </div>
-//     );
-// };
+    return (
+        <div className='sm-w-[425px] md:max-w-7xl mx-auto '>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6'>
+               {
+                equipments.map((equipment) => (<ProductCard key={product._id} product={equipment} equipments={equipments}></ProductCard>))
+               }
+            </div>
+        </div>
+    );
+};
 
-// export default Products;
+export default Products;
